c8d21442b48eefab076e54f8abebfe1cde367cd1
Nilay Namit <109901909+LuckyNilay15@users.noreply.github.com>
2025-07-04 22:51:38 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,12 +3,14 @@ import Home from "./home/Home";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Courses from "./courses/Courses";
 import Signup from "./components/Signup";
+import Chat from "./components/Chat";
 import { Toaster } from "react-hot-toast";
 import { useAuth } from "./context/AuthProvider";
+import Notes from "./components/Notes";
 
 function App() {
   const [authUser, setAuthUser] = useAuth();
-  console.log(authUser);
+  //console.log(authUser);
   return (
     <>
       <div className="dark:bg-slate-900 dark:text-white">
@@ -18,6 +20,11 @@ function App() {
             path="/course"
             element={authUser ? <Courses /> : <Navigate to="/signup" />}
           />
+           <Route
+            path="/notes"
+            element={authUser ? <Notes /> : <Navigate to="/signup" />}
+          />
+          <Route path="/chat" element={<Chat/>} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
         <Toaster />
